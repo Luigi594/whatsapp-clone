888598722b7f32d8d921d4bb1abb125b58db68e2
= <=>
2022-04-26 18:10:44 -0600

    Firsts steps of whatsapp cloning

---
+++ src/Firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAadgNte4oZHKZrWBBBpP5aGqLzlw3tunI",
+    authDomain: "whatsapp-mern-c07c6.firebaseapp.com",
+    projectId: "whatsapp-mern-c07c6",
+    storageBucket: "whatsapp-mern-c07c6.appspot.com",
+    messagingSenderId: "486417220096",
+    appId: "1:486417220096:web:be0f15ef5807b0d448cee5"
+  };
+
+  
